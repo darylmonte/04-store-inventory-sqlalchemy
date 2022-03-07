c631190f19a20304acf0c15759f6f0e70943390c
Daryl Monte <darylmonte@gmail.com>
2022-03-07 18:42:50 -0500

    flake8 edits and update readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# 04-store-inventory-sqlalchemy
+# Store Inventory with SQLAlchemy
+
+I used my knowledge of CSV and File I/O and database ORMs to build a console application that allows users to easily interact with data for a store's inventory.
  
--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def clean_date(date_str):
     return datetime.date(year, month, day)
 
 
-# clean the date
+# check for id error
 def clean_id(id_str, options):
     try:
         product_id = int(id_str)
@@ -48,23 +48,23 @@ def clean_id(id_str, options):
                 \rOptions: ({options[0]} to {options[-1]})
                 \rPress enter to try again
                 \r*************************''')
-            return        
+            return
 
 
-# add the csc to the database
+# add the csv to the database
 def add_csv():
     with open('inventory.csv') as csvfile:
         data = csv.DictReader(csvfile)
         for row in data:
             products = session.query(Product).filter(Product.product_name == row['product_name']).one_or_none()
-            if products == None:
+            if products is None:
                 name = row['product_name']
                 price = clean_price(row['product_price'].split('$')[1])
                 quantity = row['product_quantity']
                 date = clean_date(row['date_updated'])
                 new_product = Product(product_name=name, product_price=price, product_quantity=quantity, date_updated=date)
                 session.add(new_product)
-            elif products != None:
+            elif products is not None:
                 new_date = clean_date(row['date_updated'])
                 duplicate_product = session.query(Product).filter(Product.product_name == row['product_name']).first()
                 if products.product_name == duplicate_product.product_name and duplicate_product.date_updated <= new_date:
@@ -74,7 +74,7 @@ def add_csv():
         session.commit()
 
 
-# main menu - add, search, analysis, exit, view
+# main menu - view, add, create backup, exit
 def menu():
     while True:
         print('''
@@ -98,7 +98,8 @@ def app():
     app_running = True
     while app_running:
         choice = menu()
-        
+
+        # View the Product
         if choice == 'v':
             options = []
             for item in session.query(Product):
@@ -110,7 +111,7 @@ def app():
                 id_choice = clean_id(choice_id, options)
                 if id_choice:
                     id_error = False
-            
+
             item = session.query(Product).filter(Product.product_id == id_choice).first()
             print(f'''
                 \r***** {item.product_name} *****
@@ -120,9 +121,11 @@ def app():
             print('*' * 20)
             input('\nPress ENTER to continue...')
 
+        # Add a Product
         elif choice == 'a':
+            # Ask for product name
             product_name = input('What is the name of the product? ')
-
+            # Ask for quantity
             quantity_error = True
             while quantity_error:
                 try:
@@ -136,22 +139,23 @@ def app():
                 else:
                     if type(product_quantity) == int:
                         quantity_error = False
-
+            # Ask for price
             price_error = True
             while price_error:
                 price = input('How much is the product? (Ex: 11.09) ')
                 price = clean_price(price)
                 if price:
                     price_error = False
-
+            # Add date automatically
             date = datetime.date.today()
 
             new_product = Product(product_name=product_name, product_quantity=product_quantity, product_price=price, date_updated=date)
 
+            # Checking for duplicate
             product_names = []
             for product in session.query(Product):
                 product_names.append(product.product_name)
-            
+
             if new_product.product_name in product_names:
                 for product in session.query(Product):
                     if (new_product.product_name == product.product_name and new_product.date_updated >= product.date_updated):
@@ -168,26 +172,29 @@ def app():
                 input(f'''
                     \r*** {product_name} added ***
                     \rPress ENTER to continue...''')
-            
+
             session.add(new_product)
             session.commit()
 
+        # Create a Backup of the DB as csv
         elif choice == 'b':
             with open('new_inventory.csv', 'a') as csv_file:
                 header = ['product_name', 'product_price', 'product_quantity', 'date_updated']
                 writer = csv.writer(csv_file)
                 writer.writerow(header)
                 for product in session.query(Product):
-                    items = [product.product_name, product.product_price/100, product.product_quantity,product.date_updated]
+                    items = [product.product_name, product.product_price/100, product.product_quantity, product.date_updated]
                     writer.writerow(items)
                 print('Backing up data...')
                 time.sleep(1.5)
                 input('''
                     \n*** New backup CSV file created ***
                     \rPress ENTER to continue ''')
+
+        # Quit the App
         else:
             print('\nGoodbye. Thank you for using our app.\n')
-            app_running = False        
+            app_running = False
 
 
 if __name__ == '__main__':
